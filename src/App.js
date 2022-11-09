9c979a7d63c7551270f2dd17fdb59496489f05f2
pohuliaieva <bratkevich.e.a@gmail.com>
2022-11-09 17:31:34 +0000

    Added html

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css'
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Weather />
+
     </div >
   );
 }
